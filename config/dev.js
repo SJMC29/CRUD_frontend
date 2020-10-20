2b000572c9770448931bd27548ab5ef34bb75efd
Sara Maradiago <70499799+SJMC29@users.noreply.github.com>
2020-10-19 21:13:31 -0500

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    posgresqlURI: 'postgres://gahqhmjitefezb:e7d478b9bdbb2c88d449a2567f2d88deca171e9c5894204e3fde62ffd0b11c7d@ec2-54-158-222-248.compute-1.amazonaws.com:5432/dfhsqn5nmtdsbj',
+  };
